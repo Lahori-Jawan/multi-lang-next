5d8d4cf82750c3a174c9fd6e239aac38ab0d1a91
Lahori-Jawan <virgodgreat@gmail.com>
2023-03-14 15:22:48 +0500

    updated config vars

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,34 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
+import { DEFAULT_LOCALE } from "@constants/misc";
 
-const PUBLIC_FILE = /\.(.*)$/
+const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(req: NextRequest) {
-  const skip = skipRoutes(req)
-  
-  if(skip) return
+	const skip = skipRoutes(req);
 
-  const response = prefixRoute(req)
+	if (skip) return;
 
-  return response
+	const response = prefixRoute(req);
+
+	return response;
 }
 
 function skipRoutes(req: NextRequest) {
-  if (
-    req.nextUrl.pathname.startsWith('/_next') ||
-    req.nextUrl.pathname.includes('/api/') ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return true
-  }
+	if (
+		req.nextUrl.pathname.startsWith("/_next") ||
+		req.nextUrl.pathname.includes("/api/") ||
+		PUBLIC_FILE.test(req.nextUrl.pathname)
+	) {
+		return true;
+	}
 }
 
 function prefixRoute(req: NextRequest) {
-  if (req.nextUrl.locale === 'default') {
-    const locale = req.cookies.get('NEXT_LOCALE') || 'en'
+	if (req.nextUrl.locale === "default") {
+		const locale = req.cookies.get("NEXT_LOCALE") || DEFAULT_LOCALE;
 
-    return NextResponse.redirect(
-      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-    )
-  }
+		return NextResponse.redirect(
+			new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+		);
+	}
 }
